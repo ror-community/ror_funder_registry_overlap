0eda648374e7f8a49257040caa466341e61a9b7e
adambuttrick <adambuttrick@gmail.com>
2023-07-11 15:38:42 -0700

    Switching default view to aggregate and re-ordering views in the sidebar

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ from views.aggregrate_view import aggregrate_view
 from views.funder_lookup_view import funder_lookup_view
 
 views = {
-    "Overlap by member": member_view,
-    "Aggregrate overlap": aggregrate_view,
-    "Funder Mapping": funder_lookup_view
+	"Funder Mapping": funder_lookup_view,
+	"Aggregrate overlap": aggregrate_view,
+	"Overlap by member": member_view
 }
 
 
 def main():
-    sidebar_title = st.sidebar.title("Views")
+	sidebar_title = st.sidebar.title("Views")
 
-    state = st.session_state
-    if 'current_view' not in state:
-        state['current_view'] = list(views.keys())[0]
-    for view_name in views.keys():
-        if st.sidebar.button(view_name):
-            state['current_view'] = view_name
-    views[state['current_view']]()
+	state = st.session_state
+	if 'current_view' not in state:
+		state['current_view'] = list(views.keys())[1]
+	for view_name in views.keys():
+		if st.sidebar.button(view_name):
+			state['current_view'] = view_name
+	views[state['current_view']]()
 
 
 if __name__ == '__main__':
-    st.set_page_config(initial_sidebar_state="collapsed")
-    main()
+	st.set_page_config(initial_sidebar_state="collapsed")
+	main()
